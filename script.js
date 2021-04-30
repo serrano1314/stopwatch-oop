47dceb1127a05ad588310bd760c60eaf2e5773d0
Steven Serrano <steven.serrano@tup.edu.ph>
2021-04-30 20:14:41 +0800

    add throw error

--- script.js
+++ script.js
@@ -4,13 +4,19 @@ function Stopwatch(){
     let timeId = 0;
 
     this.start = ()=>{
-        timeId = setInterval(()=>{
-            duration++;
-            console.log(duration);
-        },1000);
+        if(timeId) throw new Error('STAPWATS ALREADY RUNNING')
+        else {
+            timeId = setInterval(()=>{
+                duration++;
+            },1000);
+        }
     }
     this.stop = ()=>{
-        clearInterval(timeId);
+        if(timeId===0) throw new Error('STAPWATS IS ALREADY STAPPED');
+        else {
+            clearInterval(timeId);
+            timeId=0;
+        }
     }
     this.reset = ()=>{
         duration=0;
@@ -19,4 +25,5 @@ function Stopwatch(){
         get: ()=> duration
     })
 }
+
 let sw = new Stopwatch();
